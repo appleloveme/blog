25a332a2759eb8a5a580cdc56ecc6e3250df410a
dengdeng <553192215@qq.com>
2020-06-05 13:13:31 +0800

    rebuilding site 2020年 6月 5日 星期五 13时13分22秒 CST

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+printf "开始生成博客网站并上传github"
 # Go To Public folder
 cd ../public
 
@@ -23,6 +24,8 @@ git commit -m "$msg"
 # Push source and build repos.
 git push
 
+
+printf "开始上传博客源代码"
 # Go to blog source code
 cd ../blog
 
